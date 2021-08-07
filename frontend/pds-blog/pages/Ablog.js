439255707a8bf3322b804ee91f7fe9b0e99c29c6
ydrea <andrija.wrld@gmail.com>
2021-08-07 17:11:14 +0200

    blog axiosed

---
+++ frontend/pds-blog/pages/Ablog.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Ablog() {
+    return (
+        <div>
+            do blog!
+        </div>
+    )
+}
+
+export default Ablog
---
+++ frontend/pds-blog/pages/index.js
@@ -0,0 +1,27 @@
+import axios from 'axios';
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+function Home({posts}) {
+  return (
+   <> 
+<h3>   {posts[0].title}</h3>
+
+<h3>   {posts[1].title}</h3>
+   </>
+     )
+}
+
+
+export default Home;
+
+export async function getStaticProps(){
+  const aRes = await axios.get('http://localhost:1337/blogs');
+  
+  return {
+    props: {
+      posts: aRes.data,
+    }
+  }
+} 
